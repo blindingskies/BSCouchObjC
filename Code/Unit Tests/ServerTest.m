5b20c5c8e1f68382e84b079d81a1437ffa69713c
Daniel Thorpe <dan@blindingskies.com>
2011-01-12 11:00:06 +0000

    Adding NSLog statements at the start of each test.
    Signed-off-by: Daniel Thorpe <dan@blindingskies.com>

--- Code/Unit Tests/ServerTest.m
+++ Code/Unit Tests/ServerTest.m
@@ -28,21 +28,25 @@
 }
 
 - (void)testSupportedVersion {
+    NSLog(@"Testing Server : supported version");    
     NSString *version = [server version];
     NSArray *v = [version componentsSeparatedByString: @"."];
     GHAssertTrue([[v objectAtIndex: 0] integerValue] >= 1 || [[v objectAtIndex: 1] integerValue] >= 8, @"CouchDB version %@ not supported", version);
 }
 
 - (void)testAllDatabases {
+    NSLog(@"Testing Server : all databases");    
 	NSArray *allDatabases = [server allDatabases];
 	NSLog(@"allDatabases: %@", [allDatabases description]);
 }
 
 - (void)testLogin {
+    NSLog(@"Testing Server : login");    
 	GHAssertTrue([server loginUsingName:@"administrator" andPassword:@"password"], @"Failed to log into server");
 }
 
 - (void)testDatabaseCreateAndDelete {
+    NSLog(@"Testing Server : database creation & deletion");
 	// Get a random name
 	NSString *databaseName = [NSString stringWithFormat:@"testdb%u", arc4random()];
 	server.login = @"administrator";
