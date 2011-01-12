77e84b18f07657d78eafb654a3fca319c146949f
Daniel Thorpe <dan@blindingskies.com>
2011-01-12 12:04:36 +0000

    Adding COUCH_KEY #defines from SB's CouchObjC. Also added preprocessor directives to include JSON framework. Also added extra BSCouchObjC headers.
    Signed-off-by: Daniel Thorpe <dan@blindingskies.com>

--- Code/src/BSCouchObjC.h
+++ Code/src/BSCouchObjC.h
@@ -7,7 +7,40 @@
  *
  */
 
+#define COUCH_KEY_LANGUAGE         @"language"
+#define COUCH_KEY_LANGUAGE_DEFAULT @"javascript"
+#define COUCH_KEY_DESIGN_PREFIX    @"_design/"
+#define COUCH_KEY_VIEWS            @"views"
+#define COUCH_KEY_MAP              @"map"
+#define COUCH_KEY_REDUCE           @"reduce"
+#define COUCH_KEY_ID               @"_id"
+#define COUCH_KEY_REV              @"_rev"
+#define COUCH_KEY_REVS_INFO        @"_revs_info"
+#define COUCH_KEY_REVISIONS        @"_revisions"
+#define COUCH_KEY_IDS              @"ids"
+#define COUCH_VIEW_SLOW            @"_slow_view"
+#define COUCH_VIEW_ALL             @"_all_docs"
+
+#if TARGET_OS_MAC && !(TARGET_OS_EMBEDDED || TARGET_OS_IPHONE)
+
+// OS X only headers
+#import <JSON/JSON.h>
+
+#else
+
+// iOS Headers
+#import "SBJsonParser.h"
+#import "SBJsonWriter.h"
+#import "SBJsonStreamWriter.h"
+#import "SBJsonStreamParser.h"
+#import "SBJsonStreamParserAdapter.h"
+#import "NSObject+JSON.h"
+
+#endif
+
+
 #import "BSCouchDBServer.h"
 #import "BSCouchDBDatabase.h"
 #import "BSCouchDBDocument.h"
 #import "BSCouchDBResponse.h"
+
